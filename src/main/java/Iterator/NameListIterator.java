fbb3430cc019a22d06d87ee7e36a75f843ad02da
Luis Fhilipe Loiola <luisfhilipegomesloiola@gmail.com>
2025-07-18 08:36:07 -0300

    Interface do iterator e da coleção implementadas 2

---
+++ src/main/java/Iterator/NameListIterator.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Iterator;
+
+/**
+ *
+ * @author Loiola
+ */
+public class NameListIterator implements Iterator {
+
+    private String[] nomes;
+    private int indice = 0;
+
+    public NameListIterator(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return indice < nomes.length; 
+    }
+
+    @Override
+    public String next() {
+        return hasNext() ? nomes[indice++] : null;
+    }
+
+}
