79f65a7b07d5ecfc3a284336ce38902f5ba3cccb
Luis Fhilipe Loiola <luisfhilipegomesloiola@gmail.com>
2025-07-20 10:00:21 -0300

    Commit final (Pablo e Iago)

--- src/main/java/br/edu/ifnmg/lfgl/seminario/Mediator.java
+++ src/main/java/br/edu/ifnmg/lfgl/seminario/Mediator.java
@@ -2,16 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package br.edu.ifnmg.lfgl.seminarioiterator;
+package br.edu.ifnmg.lfgl.seminario;
 
 /**
  *
  * @author Loiola
  */
-public interface Iterator {
+public interface Mediator {
 
-    boolean hasNext();
-
-    String next();
-
+    void notify(Component sender, String event);
 }
